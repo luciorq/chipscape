310b8ff19f4e267546de018e2fcff310fd782ce0
luciorq <luciorqueiroz@gmail.com>
2023-10-23 17:39:20 -0400

    feat: add bedgraph argument as optional in run_macs

--- R/run_macs.R
+++ R/run_macs.R
@@ -3,10 +3,11 @@
 #' @param sample_name The name of the sample. This value will be used
 #'   as the prefix for the output files and output directory.
 #'
-#' @param treatment_bam The path to the treatment BAM file.
+#' @param treatment_bam The path to the treatment BAM file. This argument
+#'   accepts a vector of paths.
 #'
 #' @param control_bam The path to the control BAM file. If `NULL` then
-#'   no control is used.
+#'   no control is used. This argument accepts a vector of paths.
 #'
 #' @param cutoff The cutoff to use for peak calling. Defaults to `0.05`.
 #'
@@ -17,6 +18,16 @@
 #'
 #' @param model Logical. Whether to model the fragment length.
 #'    For paired-end samples this should be `FALSE`. Defaults to `TRUE`.
+#'
+#' @param bedgraph Logical. Whether to output the bedgraph file.
+#'   Defaults to `FALSE`.
+#'
+#' @param genome The genome to use. Defaults to `hs`. If a non-model organism is
+#'   used, please use the genome length as stated in MACS2 documentation.
+#'
+#' @param input_format The input format. Defaults to `BAMPE` for BAM from
+#'   paired-end experiment. Other options are are stated in the MACS2
+#'   documentation.
 #'
 #' @param env_name The name of the Conda environment to use.
 #'
@@ -29,6 +40,7 @@ run_macs2 <- function(
     broad_cutoff = 0.1,
     broad = FALSE,
     model = TRUE,
+    bedgraph = FALSE,
     genome = "hs",
     input_format = "BAMPE",
     env_name = "macs2-env") {
@@ -58,7 +70,12 @@ run_macs2 <- function(
   } else {
     model_args <- NULL
   }
-
+  bdg_arg <- NULL
+  if (isTRUE(bedgraph)) {
+    bdg_arg <- c(
+      "--bdg"
+    )
+  }
   if (isTRUE(is.null(control_bam))) {
     control_args <- NULL
   } else {
@@ -69,7 +86,7 @@ run_macs2 <- function(
     "-t", treatment_bam,
     control_args,
     "--format", input_format,
-    "--bdg",
+    bdg_arg,
     "-g", genome,
     "-q", cutoff,
     summit_args,
